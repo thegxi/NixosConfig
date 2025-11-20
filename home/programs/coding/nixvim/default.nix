1596b18e2711d89859178ac6808d62a7732281d1
xi <village.cowherd@gmail.com>
2025-11-20 23:02:03 +0800

    feat: nixvim lsp configuration

--- home/programs/coding/nixvim/default.nix
+++ home/programs/coding/nixvim/default.nix
@@ -9,9 +9,11 @@
     plugins.web-devicons.enable = true;
     imports = [
       ./bufferline.nix
+      ./cmp.nix
       ./keymaps.nix
       ./neo-tree.nix
       ./options.nix
+      ./lsp.nix
       ./treesitter.nix
       ./ui.nix
     ];
--- home/programs/coding/nixvim/lsp.nix
+++ home/programs/coding/nixvim/lsp.nix
@@ -1,128 +1,161 @@
+{ pkgs, ... }:
 {
-  pkgs,
-  lib,
-  inputs,
-  host,
-  user,
-  ...
-}:
-{
-  programs.nixvim = {
-    plugins = {
-      lsp = {
-        enable = true;
-        inlayHints = false;
-        keymaps = {
-          diagnostic = {
-            "<leader>lE" = "open_float";
-            "[" = "goto_prev";
-            "]" = "goto_next";
-          };
-          lspBuf = {
-            "gD" = "declaration";
-            "gd" = "definition";
-            "gr" = "references";
-            "gI" = "implementation";
-            "gy" = "type_definition";
-          };
+  plugins = {
+    lsp-lines = {
+      enable = true;
+    };
+    lsp-format = {
+      enable = true;
+    };
+    helm = {
+      enable = true;
+    };
+    lsp = {
+      enable = true;
+      inlayHints = true;
+      servers = {
+        html = {
+          enable = true;
         };
-        servers = {
-          ruff.enable = true;
-          pyright.enable = true;
-          lua_ls.enable = true;
-          texlab.enable = true;
-          harper_ls.enable = true;
+        lua_ls = {
+          enable = true;
         };
-      };
-      conform-nvim = {
-        enable = true;
-        settings = {
-          format_on_save = {
-            lsp_fallback = "fallback";
-            timeout_ms = 500;
-          };
-          notify_on_error = true;
-
-          formatters_by_ft = {
-            bash = [
-              "shfmt"
-              "shellcheck"
-              "shellharden"
-            ];
-            css = [ "prettier" ];
-            scss = [ "prettier" ];
-            html = [ "prettier" ];
-            json = [ "prettier" ];
-            jsonc = [ "prettier" ];
-            lua = [ "stylua" ];
-            markdown = [
-              "prettier"
-              "injected"
-            ];
-            nix = [
-              "nixfmt"
-              "injected"
-            ];
-            yaml = [ "yamlfmt" ];
-            python = [ "ruff" ];
-            tex = [ "latexindent" ];
-          };
-          formatters = {
-            injeced.lang_to_ext = {
-              lua = "lua";
+        nil_ls = {
+          enable = true;
+        };
+        ts_ls = {
+          enable = true;
+        };
+        marksman = {
+          enable = true;
+        };
+        pyright = {
+          enable = true;
+        };
+        gopls = {
+          enable = true;
+        };
+        terraformls = {
+          enable = true;
+        };
+        jsonls = {
+          enable = true;
+        };
+        helm_ls = {
+          enable = true;
+          extraOptions = {
+            settings = {
+              "helm_ls" = {
+                yamlls = {
+                  path = "${pkgs.yaml-language-server}/bin/yaml-language-server";
+                };
+              };
             };
-            shfmt.command = lib.getExe pkgs.shfmt;
-            shellcheck.command = lib.getExe pkgs.shellcheck;
-            shellharden.command = lib.getExe pkgs.shellharden;
-            stylua.command = lib.getExe pkgs.stylua;
-            yamlfmt.command = lib.getExe pkgs.yamlfmt;
-            latexindent.prepend_args = [ ''-y="defaultIndent='  '"'' ];
+          };
+        };
+        yamlls = {
+          enable = true;
+          extraOptions = {
+            settings = {
+              yaml = {
+                schemas = {
+                  kubernetes = "'*.yaml";
+                  "http://json.schemastore.org/github-workflow" = ".github/workflows/*";
+                  "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
+                  "http://json.schemastore.org/ansible-stable-2.9" = "roles/tasks/*.{yml,yaml}";
+                  "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
+                  "http://json.schemastore.org/ansible-playbook" = "*play*.{yml,yaml}";
+                  "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
+                  "https://json.schemastore.org/dependabot-v2" = ".github/dependabot.{yml,yaml}";
+                  "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" =
+                    "*docker-compose*.{yml,yaml}";
+                  "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" =
+                    "*flow*.{yml,yaml}";
+                };
+              };
+            };
           };
         };
       };
-      lsp-format = {
-        enable = true;
-      };
-      lspsaga = {
-        enable = true;
-        settings.lightbulb = {
-          virtualText = true;
-          sign = false;
+
+      keymaps = {
+        silent = true;
+        lspBuf = {
+          gd = {
+            action = "definition";
+            desc = "Goto Definition";
+          };
+          gr = {
+            action = "references";
+            desc = "Goto References";
+          };
+          gD = {
+            action = "declaration";
+            desc = "Goto Declaration";
+          };
+          gI = {
+            action = "implementation";
+            desc = "Goto Implementation";
+          };
+          gT = {
+            action = "type_definition";
+            desc = "Type Definition";
+          };
+          K = {
+            action = "hover";
+            desc = "Hover";
+          };
+          "<leader>cw" = {
+            action = "workspace_symbol";
+            desc = "Workspace Symbol";
+          };
+          "<leader>cr" = {
+            action = "rename";
+            desc = "Rename";
+          };
+        };
+        diagnostic = {
+          "<leader>cd" = {
+            action = "open_float";
+            desc = "Line Diagnostics";
+          };
+          "[d" = {
+            action = "goto_next";
+            desc = "Next Diagnostic";
+          };
+          "]d" = {
+            action = "goto_prev";
+            desc = "Previous Diagnostic";
+          };
         };
       };
-      trouble.enable = true;
-      lsp-signature.enable = true;
-      otter.enable = true;
     };
   };
-  programs.nixvim.keymaps = [
-    {
-      mode = "n";
-      key = "K";
-      action = ":Lspsaga hover_doc<CR>";
-    }
-    {
-      mode = "n";
-      key = "<Leader>lo";
-      action = ":Lspsaga outline<CR>";
-    }
-    {
-      mode = "n";
-      key = "<Leader>lr";
-      action = ":Lspsaga rename<CR>";
-    }
-    {
-      mode = "n";
-      key = "<Leader>la";
-      action = ":Lspsaga code_action<CR>";
-    }
-    {
-      mode = "n";
-      key = "<Leader>lf";
-      action = ":Lspsaga finder<CR>";
-    }
+  extraPlugins = with pkgs.vimPlugins; [
+    ansible-vim
   ];
-  programs.nixvim.extraPlugins = with pkgs.vimPlugins; [
-    nvim-docs-view
-  ];
+
+  extraConfigLua = ''
+    local _border = "rounded"
+
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+      vim.lsp.handlers.hover, {
+        border = _border
+      }
+    )
+
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+      vim.lsp.handlers.signature_help, {
+        border = _border
+      }
+    )
+
+    vim.diagnostic.config{
+      float={border=_border}
+    };
+
+    require('lspconfig.ui.windows').default_options = {
+      border = _border
+    }
+  '';
 }
